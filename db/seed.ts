f84f9d63603ac4808481e25d1da312f2fe07308b
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-04-19 08:53:41 -0400

    Add newTeam API route and join page

--- db/seed.ts
+++ db/seed.ts
@@ -2,10 +2,6 @@ import { db, User, Organization, Event } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	await db.insert(Organization).values([
-		{ team: 'T0266FRGM', name: 'Hackclub', image: 'https://avatars.slack-edge.com/2022-04-01/3330920659891_26353517af684373601b_230.png' },
-	]);
-
 	await db.insert(Event).values([
 		{ team: 'T0266FRGM', name: 'Hackclub Meeting', comments: 'This is a test event', date: new Date(), location: 'Some location', statusGoing: '', statusMaybe: '', statusNotGoing: 'U062UG485EE' },
 	]);
