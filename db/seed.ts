10c7f7190765dfb607cd52353d850125479a3754
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-04-27 15:10:01 -0400

    Update team ID in seed.ts to include "slack-" prefix

--- db/seed.ts
+++ db/seed.ts
@@ -12,7 +12,7 @@ export default async function seed() {
   ]);
   await db.insert(Event).values([
     {
-      team: "T0266FRGM",
+      team: "slack-T0266FRGM",
       name: "Hackclub Meeting",
       comments: "This is a test event",
       date: new Date(),
